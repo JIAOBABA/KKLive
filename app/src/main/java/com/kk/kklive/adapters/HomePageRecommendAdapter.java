428be71a6ac435d60258593594cdf9221bf23119
fei <935189950@qq.com>
2016-09-22 16:47:11 +0800

    首页推荐

--- app/src/main/java/com/kk/kklive/adapters/HomePageRecommendAdapter.java
+++ app/src/main/java/com/kk/kklive/adapters/HomePageRecommendAdapter.java
@@ -5,35 +5,54 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kk.kklive.R;
+import com.kk.kklive.model.Hot;
 import com.kk.kklive.model.Recommend;
+
+import org.xutils.image.ImageOptions;
+import org.xutils.x;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
+
 /**
  * Created by fei on 2016/9/20.
  */
-public class HomePageRecommendAdapter extends RecyclerView.Adapter<HomePageRecommendAdapter.ViewHolder> {
+public class HomePageRecommendAdapter extends BaseAdapter implements StickyListHeadersAdapter, View.OnClickListener {
 
     private List<Recommend.RoomListBean> data;
     private LayoutInflater inflater;
+    private ImageOptions mOptions;
+    private OnItemClickListener mListener;
 
-    public HomePageRecommendAdapter(Context context, List<Recommend.RoomListBean> data){
+    public void setListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    public HomePageRecommendAdapter(Context context, List<Recommend.RoomListBean> data) {
         inflater = LayoutInflater.from(context);
         if (data!=null) {
             this.data = data;
         }else {
             this.data = new ArrayList<>();
         }
+        mOptions = new ImageOptions.Builder()
+                .setSquare(true)
+                .setRadius(10)
+                .setLoadingDrawableId(R.mipmap.kk_room_honor_default_icon)
+                .setFailureDrawableId(R.mipmap.kk_room_honor_none_img)
+                .build();
     }
 
-    public void updataRes(List<Recommend.RoomListBean> data){
+    public void updateRes(List<Recommend.RoomListBean> data){
         if (data!=null) {
             this.data.clear();
             this.data.addAll(data);
@@ -41,44 +60,112 @@ public class HomePageRecommendAdapter extends RecyclerView.Adapter<HomePageRecom
         }
     }
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView =  inflater.inflate(R.layout.item_recommend,parent,false);
-        return new ViewHolder(itemView);
+    public void addRes(List<Recommend.RoomListBean> data){
+        if (data!=null) {
+            this.data.addAll(data);
+            notifyDataSetChanged();
+        }
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        ImageView image = (ImageView) holder.getView(R.id.item_recommend_image);
-        TextView nickName = (TextView) holder.getView(R.id.item_recommend_nickname);
-        TextView onLineCount = (TextView) holder.getView(R.id.item_recommend_online_count);
-        nickName.setText(data.get(position).getNickname());
-        onLineCount.setText(String.valueOf(data.get(position).getOnlineCount()));
+    public int getCount() {
+        return data!=null ? (data.size() + 1)/2 : 0;
     }
 
     @Override
-    public int getItemCount() {
-        return data!=null ? data.size() : 0;
+    public Recommend.RoomListBean getItem(int position) {
+        return data.get(position);
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_home_page_recommend_sticky,parent,false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        ImageView icon = (ImageView) holder.getViewRes(R.id.item_home_page_recommend_icon);
+        TextView roomTheme = (TextView) holder.getViewRes(R.id.item_home_page_recommend_roomTheme);
+        TextView nickName = (TextView) holder.getViewRes(R.id.item_home_page_recommend_nickname);
+        TextView onLineCount = (TextView) holder.getViewRes(R.id.item_home_page_recommend_online_count);
+
+        ImageView icon1 = (ImageView) holder.getViewRes(R.id.item_home_page_recommend_icon1);
+        TextView roomTheme1 = (TextView) holder.getViewRes(R.id.item_home_page_recommend_roomTheme1);
+        TextView nickName1 = (TextView) holder.getViewRes(R.id.item_home_page_recommend_nickname1);
+        TextView onLineCount1 = (TextView) holder.getViewRes(R.id.item_home_page_recommend_online_count1);
+
+        x.image().bind(icon,"http://ures.kktv8.com/kktv"+getItem(2*position).getPortrait_path_256(),mOptions);
+        roomTheme.setText(getItem(position * 2).getRoomTheme());
+        nickName.setText(getItem(2*position).getNickname());
+        onLineCount.setText(String.valueOf(getItem(2*position).getOnlineCount()));
+
+        x.image().bind(icon1,"http://ures.kktv8.com/kktv"+getItem(position*2+1).getPortrait_path_256(),mOptions);
+        roomTheme1.setText(getItem(position * 2+1).getRoomTheme());
+        nickName1.setText(getItem(position*2+1).getNickname());
+        onLineCount1.setText(String.valueOf(getItem(position*2+1).getOnlineCount()));
+
+        icon.setOnClickListener(this);
+        icon1.setOnClickListener(this);
+        icon.setTag(position*2);
+        icon1.setTag(position*2+1);
+
+        return convertView;
+    }
+
+    @Override
+    public View getHeaderView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_home_page_recommend_sticky_header,parent,false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        return convertView;
+    }
+
+    @Override
+    public long getHeaderId(int position) {
+        return 1;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Integer position = (Integer) v.getTag();
+        mListener.onItemClick(position);
+    }
+
+    class ViewHolder{
+        private View convertView;
         private Map<Integer,View> mCacheViews;
 
-        public ViewHolder(View itemView) {
-            super(itemView);
+        public ViewHolder(View convertView){
+            this.convertView = convertView;
             mCacheViews = new HashMap<>();
         }
 
-        public View getView(int resId){
+        public View getViewRes(int resId){
             View view = null;
             if (mCacheViews.containsKey(resId)) {
                 view = mCacheViews.get(resId);
             }else {
-                view = itemView.findViewById(resId);
+                view = convertView.findViewById(resId);
                 mCacheViews.put(resId,view);
             }
             return view;
         }
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
     }
 }
--- app/src/main/java/com/kk/kklive/ui/fragments/HomePageFragment.java
+++ app/src/main/java/com/kk/kklive/ui/fragments/HomePageFragment.java
@@ -8,9 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
 import com.kk.kklive.R;
 import com.kk.kklive.adapters.HomePageRecommendAdapter;
 import com.kk.kklive.model.Recommend;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,8 +28,9 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
  */
 public class HomePageFragment extends BaseFragment {
 
-    private RecyclerView mRecyclerView;
+    private static final String HOMEPAGERECOMMEND_URL = "http://api.kktv1.com:8080/meShow/entrance?parameter={\"a\":1,\"c\":70036,\"FuncTag\":55000002,\"start\":0,\"offset\":14,\"platform\":2}";
     private StickyListHeadersListView mStickyListHeadersListView;
+    private HomePageRecommendAdapter mAdapter;
 
     @Nullable
     @Override
@@ -37,11 +43,41 @@ public class HomePageFragment extends BaseFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initView();
+        setupView();
+    }
+
+    private void setupView() {
+        RequestParams params = new RequestParams(HOMEPAGERECOMMEND_URL);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Gson gson = new Gson();
+                Recommend recommend = gson.fromJson(result, Recommend.class);
+                List<Recommend.RoomListBean> roomList = recommend.getRoomList();
+                mAdapter.updateRes(roomList);
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
     }
 
     private void initView() {
         mStickyListHeadersListView = (StickyListHeadersListView) layout.findViewById(R.id.home_page_sticky_list);
-
+        mAdapter = new HomePageRecommendAdapter(getActivity(),null);
+        mStickyListHeadersListView.setAdapter(mAdapter);
     }
 
 }
